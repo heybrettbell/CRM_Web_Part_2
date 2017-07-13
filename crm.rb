51e5652955bb05ea8abffbfb5d4a387db42d43b4
heybrettbell <brett@grassrootsonline.ca>
2017-07-13 18:15:33 -0400

    Part 2 of the assignment

--- crm.rb
+++ crm.rb
@@ -2,7 +2,7 @@ require_relative 'contact'
 require 'sinatra'
 
 get '/' do
-  erb :index
+  redirect to ('/contacts')
 end
 
 get '/about' do
@@ -12,6 +12,16 @@ end
 get '/contacts' do
   @contacts = Contact.all
   erb :contacts
+end
+
+get '/contacts/:id' do
+  # params[:id] contains the id from the URL
+  @contact = Contact.find_by({id: params[:id].to_i})
+  if @contact
+    erb :show_contact
+  else
+   raise Sinatra::NotFound
+ end
 end
 
 after do
